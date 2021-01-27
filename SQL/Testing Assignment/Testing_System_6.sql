8f50e90daee70f0ca6e63038a8d6a3bad82bcf56
khoihoang123-zz <kephahoai1101@gmail.com>
2021-01-27 11:09:32 +0700

    Update

--- SQL/Testing Assignment/Testing_System_6.sql
+++ SQL/Testing Assignment/Testing_System_6.sql
@@ -19,7 +19,7 @@ CREATE PROCEDURE account_group ()
 BEGIN
 	SELECT t1.GroupID,COUNT(t2.AccountID) AS 'So thanh vien'
     FROM `GroupAccount` t1
-    JOIN `Account` t2 ON t1.AccountID = t2.AccountID 
+    LEFT JOIN `Account` t2 ON t1.AccountID = t2.AccountID 
     GROUP BY t1.GroupID;
 END $$
 DELIMITER ;
@@ -27,64 +27,107 @@ DELIMITER ;
 call account_group ();
 
 -- Question 3: Tạo store để thống kê mỗi type question có bao nhiêu question được tạo trong tháng hiện tại 
+DROP PROCEDURE IF EXISTS Cau_Hoi_Trong_Thang;
 DELIMITER $$
-DROP PROCEDURE IF EXISTS typequestion_question;
-CREATE PROCEDURE typequestion_question ()
+CREATE PROCEDURE Cau_Hoi_Trong_Thang ()
 BEGIN
-	SELECT t1.TypeName,COUNT(t2.TypeID) AS 'so question',t2.CreateDate
+	SELECT	t1.TypeID, t1.TypeName, COUNT(t2.QuestionID) AS So_Q
     FROM `TypeQuestion` t1
-    JOIN `Question` t2 ON t1.TypeID = t2.TypeID
-    GROUP BY t1.TypeID
-    WHERE CreateDate = in_CreateDate;
+    JOIN `Question`     t2 ON t1.TypeID = t2.TypeID
+    WHERE	MONTH(t2.CreateDate) = MONTH(NOW())
+		AND YEAR(t2.CreateDate) = YEAR(NOW())
+    GROUP BY	t1.TypeID
+    ;
 END $$
 DELIMITER ;
 
--- Question 4: Tạo store để trả ra id của type question có nhiều câu hỏi nhất 
+CALL Cau_Hoi_Trong_Thang();
+
+-- Question 4: Tạo store để trả ra id của type question có nhiều câu hỏi nhất
+DROP PROCEDURE IF EXISTS TypeID_MaxQ;
 DELIMITER $$
-DROP PROCEDURE IF EXISTS maxqoftypequestion;
-CREATE PROCEDURE maxqoftypequestion ()
+
+CREATE PROCEDURE TypeID_MaxQ()
 BEGIN
-	SELECT t1.TypeID,COUNT(t2.TypeID) AS 'so question'
-    FROM `TypeQuestion` t1
+	SELECT t1.TypeID , count(t2.TypeID)
+	FROM `TypeQuestion` t1
     JOIN `Question` t2 ON t1.TypeID = t2.TypeID
-    GROUP BY t1.TypeID HAVING COUNT(t2.TypeID) = (SELECT MAX(countt2) FROM (SELECT COUNT(t2.TypeID) AS countt2
-												FROM `TypeQuestion` t1
-                                                JOIN `Question` t2 ON t1.TypeID = t2.TypeID 
-                                                GROUP BY t2.TypeID ) AS MaxCountt2);
+    GROUP BY t2.TypeID
+    HAVING count(t2.TypeID) = (	SELECT MAX(NumberOfQuestion)
+										FROM (	SELECT COUNT(t2.TypeID) AS NumberOfQuestion
+												FROM 	`Question`
+                                                GROUP BY t2.TypeID) );
 END $$
+
 DELIMITER ;
 
-call maxqoftypequestion ();
-
--- Question 5: Sử dụng store ở question 4 để tìm ra tên của type question 
+-- Question 5: Sử dụng store ở question 4 để tìm ra tên của type question
+DROP PROCEDURE IF EXISTS Question5;
 DELIMITER $$
-DROP PROCEDURE IF EXISTS maxqoftypequestion;
-CREATE PROCEDURE maxqoftypequestion ()
+
+CREATE PROCEDURE Question5()
 BEGIN
-	SELECT t1.TypeID,t1.TypeName,COUNT(t2.TypeID) AS 'so question'
-    FROM `TypeQuestion` t1
-    JOIN `Question` t2 ON t1.TypeID = t2.TypeID
-    GROUP BY t1.TypeID HAVING COUNT(t2.TypeID) = (SELECT MAX(countt2) FROM (SELECT COUNT(t2.TypeID) AS countt2
-												FROM `TypeQuestion` t1
-                                                JOIN `Question` t2 ON t1.TypeID = t2.TypeID 
-                                                GROUP BY t2.TypeID ) AS MaxCountt2);
+	SELECT typequestion.TypeID ,typequestion.TypeName , count(question.TypeID)
+	FROM question
+    JOIN typequestion ON question.TypeID = typequestion.TypeID
+    GROUP BY typequestion.TypeID
+    HAVING count(question.TypeID) = (	SELECT MAX(Number_Of_Question)
+										FROM (	SELECT count(question.TypeID) AS Number_Of_Question
+												FROM 	question
+                                                GROUP BY question.TypeID) e1 );
 END $$
+
+-- Question 6: Viết 1 store cho phép người dùng nhập vào 1 chuỗi và trả về group có tên chứa chuỗi của người dùng nhập vào hoặc trả về user có username chứa chuỗi của người dùng nhập vào
+DROP PROCEDURE IF EXISTS Question6;
+DELIMITER $$
+CREATE PROCEDURE Question6( IN Nhap Varchar(30), IN Group_OR_User ENUM('group','user'))
+Begin
+	IF Group_OR_User = 'group'
+	THEN	 SELECT GroupName 
+			 FROM `group`
+             Where	GroupName Like concat('%' , Nhap ,'%');
+	Else
+			SELECT AccountID, Email, Username, Fullname
+            From `account`	
+            Where Username like concat('%' , Nhap ,'%');
+	END IF;
+    END$$
+    
 DELIMITER ;
 
-call maxqoftypequestion();
 
--- Question 6: Viết 1 store cho phép người dùng nhập vào 1 chuỗi và trả về group có tên chứa chuỗi của người dùng nhập vào hoặc trả về user có username chứa chuỗi của người dùng nhập vào 
+-- Question 7: Viết 1 store cho phép người dùng nhập vào thông tin fullName, email và trong store sẽ tự động gán: 
+-- username sẽ giống email nhưng bỏ phần @..mail đi 
+-- positionID: sẽ có default là developer 
+-- departmentID: sẽ được cho vào 1 phòng chờ 
+-- 	Sau đó in ra kết quả tạo thành công 
+
+DROP PROCEDURE IF EXISTS insert_account;
 DELIMITER $$
-DROP PROCEDURE IF EXISTS GroupName;
-CREATE PROCEDURE GroupName(IN in_FullName VARCHAR (30),OUT out_FullName VARCHAR(30))
+CREATE PROCEDURE insert_account(IN in_full_name VARCHAR(255), IN in_email VARCHAR(100))
 BEGIN
-	SELECT FullName,Username
-    FROM `Account`
-    WHERE FullName LIKE (
-
-
-
-
+	DECLARE v_department_id INT(10) DEFAULT 0;
+    DECLARE v_position_id INT(10) DEFAULT 0;
+    
+    SELECT department_id INTO v_department_id
+    FROM departments 
+    WHERE department_name = 'Waiting Room';
+       
+	SELECT position_id INTO v_position_id
+    FROM positions 
+    WHERE position_name = 'Developer';
+    
+    INSERT INTO accounts 
+	(email, 		user_name, 								full_name, 		department_id, 		position_id) VALUES
+    (in_email, 		SUBSTRING_INDEX(in_email, '@', 1), 		in_full_name, 	v_department_id, 	v_position_id);
+    
+    IF ROW_COUNT() > 0 THEN
+		SELECT 'You created record successfully!';
+    ELSE 
+		SELECT 'You did not create record successfully!';
+	END IF;
+END$$
+DELIMITER ;
 
     
 
--- SQL/Testing Assignment/Testing_System_7.sql
+++ SQL/Testing Assignment/Testing_System_7.sql
@@ -1,3 +1,42 @@
+-- Question 1: Tạo trigger không cho phép người dùng nhập vào Group có ngày tạo trước 1 năm trước 
+DROP TRIGGER IF EXISTS 	trig_no_group_1_year;
+
+DELIMITER //
+	CREATE TRIGGER trig_no_group_1_year
+    BEFORE INSERT ON `Group`
+    FOR EACH ROW
+    BEGIN
+		IF	(NEW.CreateDate < DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)) THEN 
+        SIGNAL SQLSTATE '12345'
+        SET MESSAGE_TEXT = 'Cannot add group created for one year or previous';
+        END IF;
+    END//
+DELIMITER ;
+
+-- Question 3: Cấu hình 1 group có nhiều nhất là 5 user
+DROP TRIGGER IF EXISTS Question3;
+DELIMITER $$
+
+Create trigger Question3 Before INSERT 
+ON groupaccount
+For each row
+
+Begin
+	
+    IF New.GroupID in ( select GroupID
+						From
+						(	select GroupID, count(AccountID) 
+							From groupaccount 
+							GROUP BY GroupID
+							Having count(AccountID)=5) t1 )
+    Then	
+    signal sqlstate '45000'
+    Set message_text = 'Max five acc in each group';
+    END IF;
+End$$
+
+ DELIMITER ;
+
 
 
 
